0d87cb0355600337f1bf91305f7d3be4a4f4614a
BercesteDincer <bercestedincer@gmail.com>
2018-12-01 11:37:05 -0800

    Update Message.py

--- Message.py
+++ Message.py
@@ -5,10 +5,20 @@ class Message:
 
 #Message class for sending the request of the client to the PROPOSER
 class TrainingMsg(Message):
-    def __init__(self, source, start, end):
+    def __init__(self, source, start, end, model_file, weights_file):
         super(TrainingMsg, self).__init__(source)
         self.start = start
         self.end = end
+        self.model_file = model_file
+        self.weights_file = weights_file
     def __str__(self):
         return "TRAIN MESSAGE = [Starting = {}, Ending = {}]".format(self.start, self.end)
 
+class ClientRequestMsg(Message):
+    def __init__(self, source, model_file, weights_file, input_file):
+        super(Message, self).__init__(source)
+        self.model_file = model_file
+        self.weights_file = weights_file
+        self.input_file = input_file
+    def __str__(self):
+        return "CLIENT REQUEST MESSAGE = [Model file = {}, Weights File = {}, Input File = {}]".format(self.model_file, self.weights_file, self.input_file)
