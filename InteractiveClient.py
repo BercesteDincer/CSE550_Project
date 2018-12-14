0db44d6c7ce0576e4f3282e5421994e849fdf7ec
BercesteDincer <bercestedincer@gmail.com>
2018-12-13 19:08:17 -0800

    Update InteractiveClient.py

--- InteractiveClient.py
+++ InteractiveClient.py
@@ -1,12 +1,11 @@
-
 import socket
+import time
 
 #Define the client
 SERVER = "127.0.0.1"
 PORT = 8080
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
-client.settimeout(3)
 in_data =  client.recv(1024)
 print("From Server :" ,in_data.decode())
  
@@ -15,14 +14,13 @@ failed = 0
 while True:
   print("Enter command:")
   out_data = input()
+  start_time = time.time()
   client.sendall(bytes(out_data,'UTF-8'))
   if out_data=='bye':
     break
-  try:
-  	in_data =  client.recv(1024)
-  except socket.timeout:
-  	continue
+  in_data =  client.recv(1024)
   print("From Server :" ,in_data.decode())
+  elapsed_time = time.time() - start_time
+
+  print("TOOK ", elapsed_time)
 client.close()
-
-
